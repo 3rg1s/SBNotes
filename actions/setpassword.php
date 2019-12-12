e5c59204e2a32ebafc8eb0b8282f0230d09dfe25
fuxsocy.py <0x0byte@protonmail.com>
2019-12-12 22:53:55 +0200

    Prevent non logged in users to access this file

--- actions/setpassword.php
+++ actions/setpassword.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+     header("location: ../login.php");                                                                                                                                                                                                                  exit;                                                                                                                                                                                                                                     }
+?>
 <!DOCTYPE html>
 <html>
 <head>
