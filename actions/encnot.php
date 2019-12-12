f7929354909042df96982eadf0213ec01740a791
fuxsocy.py <0x0byte@protonmail.com>
2019-12-12 22:55:53 +0200

    OOPS

--- actions/encnot.php
+++ actions/encnot.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 // Check if the user is logged in, otherwise redirect to login page                                                                                                                                                                           
  if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
      header("location: ../login.php");                                                                                                                                                                                                                  exit;                                                                                                                                                                                                                                     }
